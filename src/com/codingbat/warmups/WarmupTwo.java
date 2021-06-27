54faaf0642247c7ed8e1eb68053e7db69a14c95b
A66ac <thea66ac@gmail.com>
2021-06-27 15:36:41 +0200

    added new solutions

--- src/com/codingbat/warmups/WarmupTwo.java
+++ src/com/codingbat/warmups/WarmupTwo.java
@@ -140,4 +140,46 @@ public class WarmupTwo {
 
         return count;
     }
+
+    /*Given an array of ints, return the number of 9's in the array.
+
+    arrayCount9([1, 2, 9]) → 1
+    arrayCount9([1, 9, 9]) → 2
+    arrayCount9([1, 9, 9, 3, 9]) → 3*/
+    public int arrayCount9(int[] nums) {
+        final int nine = 9;
+        int count = 0;
+
+        for (int num : nums)
+            if (num == nine)
+                count++;
+
+        return count;
+    }
+
+    /*Given an array of ints, return true if one of the first 4 elements in the array is a 9.
+    The array length may be less than 4.
+
+    arrayFront9([1, 2, 9, 3, 4]) → true
+    arrayFront9([1, 2, 3, 4, 9]) → false
+    arrayFront9([1, 2, 3, 4, 5]) → false*/
+    public boolean arrayFront9(int[] nums) {
+        for (int i = 0; i < nums.length; i++)
+            if (nums[i] == 9 && i < 4)
+                return true;
+
+        return false;
+    }
+
+    /*Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+    array123([1, 1, 2, 3, 1]) → true
+    array123([1, 1, 2, 4, 1]) → false
+    array123([1, 1, 2, 1, 2, 3]) → true*/
+    public boolean array123(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++)
+            if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3)
+                return true;
+        return false;
+    }
 }
