089a3296a1738cd90f7d024628925c1aa8f94db1
A66ac <thea66ac@gmail.com>
2021-07-14 12:10:08 +0200

    added 3 last to class WarmupOne

--- src/com/codingbat/warmups/WarmupOne.java
+++ src/com/codingbat/warmups/WarmupOne.java
@@ -330,4 +330,31 @@ public class WarmupOne {
     public int intMax(int a, int b, int c) {
         return Math.max(Math.max(a, b), c);
     }
+
+    /*Given 2 positive int values, return the larger value that is in the range 10..20 inclusive,
+    or return 0 if neither is in that range.
+
+    max1020(11, 19) → 19
+    max1020(19, 11) → 19
+    max1020(11, 9) → 11*/
+    public int max1020(int a, int b) {
+        a = 10 <= a && a <= 20 ? a : 0;
+        b = 10 <= b && b <= 20 ? b : 0;
+
+        return Math.max(a, b);
+    }
+
+    /*Given a string, return a new string where the last 3 chars are now in upper case. If the
+    string has less than 3 chars, uppercase whatever is there. Note that str.toUpperCase()
+    returns the uppercase version of a string.
+
+    endUp("Hello") → "HeLLO"
+    endUp("hi there") → "hi thERE"
+    endUp("hi") → "HI"*/
+    public String endUp(String str) {
+        if (str.length() < 3)
+            return str.toUpperCase();
+
+        return str.substring(0, str.length() - 3) + str.substring(str.length() - 3).toUpperCase();
+    }
 }
