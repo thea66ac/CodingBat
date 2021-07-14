9f85ee12049f0a1a3d886192524788c2cbf88069
A66ac <thea66ac@gmail.com>
2021-07-14 12:31:56 +0200

    added a new solution to the class WarmupTwo

--- src/com/codingbat/warmups/WarmupTwo.java
+++ src/com/codingbat/warmups/WarmupTwo.java
@@ -235,4 +235,24 @@ public class WarmupTwo {
 
         return strBuilder.toString();
     }
+
+    /*Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak"
+    are removed, but the "a" can be any char. The "yak" strings will not overlap.
+
+    stringYak("yakpak") → "pak"
+    stringYak("pakyak") → "pak"
+    stringYak("yak123ya") → "123ya"*/
+    public String stringYak(String str) {
+        // short solution => return str.replaceAll("y.k", "");
+        StringBuilder strBuilder = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k')
+                i += 2;
+            else
+                strBuilder.append(str.charAt(i));
+        }
+
+        return strBuilder.toString();
+    }
 }
