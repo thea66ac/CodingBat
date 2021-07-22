086dcb3c812d8854ec190efff17d0bbc9e5642ec
A66ac <thea66ac@gmail.com>
2021-07-22 12:13:51 +0200

    added 3 solutions to class ArrayOne

--- src/com/codingbat/arrays/ArrayOne.java
+++ src/com/codingbat/arrays/ArrayOne.java
@@ -47,4 +47,43 @@ public class ArrayOne {
 
         return arr;
     }
+
+    /*Given 2 arrays of ints, a and b, return true if they have the same first element or they
+    have the same last element. Both arrays will be length 1 or more.
+
+    commonEnd([1, 2, 3], [7, 3]) → true
+    commonEnd([1, 2, 3], [7, 3, 2]) → false
+    commonEnd([1, 2, 3], [1, 3]) → true*/
+    public boolean commonEnd(int[] a, int[] b) {
+        return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+    }
+
+    /*Given an array of ints length 3, return the sum of all the elements.
+
+    sum3([1, 2, 3]) → 6
+    sum3([5, 11, 2]) → 18
+    sum3([7, 0, 0]) → 7*/
+    public int sum3(int[] nums) {
+        int sum = 0;
+        for (int num : nums) sum += num;
+        return sum;
+    }
+
+    /*Given an array of ints length 3, return an array with
+    the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+
+    rotateLeft3([1, 2, 3]) → [2, 3, 1]
+    rotateLeft3([5, 11, 9]) → [11, 9, 5]
+    rotateLeft3([7, 0, 0]) → [0, 0, 7]*/
+    public int[] rotateLeft3(int[] nums) {
+        int tmp = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            tmp = nums[i];
+            nums[i] = nums[i + 1];
+            nums[i + 1] = tmp;
+        }
+
+        return nums;
+    }
 }
