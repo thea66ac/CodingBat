324a8e6c78ad62f6b654bc92a3b67a03f1dbb0ab
A66ac <thea66ac@gmail.com>
2021-07-31 17:09:15 +0200

    added 3 solutions to class ArrayOne

--- src/com/codingbat/arrays/ArrayOne.java
+++ src/com/codingbat/arrays/ArrayOne.java
@@ -1,5 +1,7 @@
 package com.codingbat.arrays;
 
+import java.util.Arrays;
+
 public class ArrayOne {
     public static void main(String[] args) {
 
@@ -77,13 +79,52 @@ public class ArrayOne {
     rotateLeft3([7, 0, 0]) → [0, 0, 7]*/
     public int[] rotateLeft3(int[] nums) {
         int tmp = 0;
-
         for (int i = 0; i < nums.length - 1; i++) {
             tmp = nums[i];
             nums[i] = nums[i + 1];
             nums[i + 1] = tmp;
         }
-
         return nums;
+    }
+
+    /*Given an array of ints length 3, return a new array with the elements in reverse order, so
+    {1, 2, 3} becomes {3, 2, 1}.
+
+    reverse3([1, 2, 3]) → [3, 2, 1]
+    reverse3([5, 11, 9]) → [9, 11, 5]
+    reverse3([7, 0, 0]) → [0, 0, 7]*/
+    public int[] reverse3(int[] nums) {
+        int tmp = nums[0];
+        nums[0] = nums[2];
+        nums[2] = tmp;
+        return nums;
+    }
+
+    /*Given an array of ints length 3, figure out which is larger, the first or last element in the
+    array, and set all the other elements to be that value. Return the changed array.
+
+    maxEnd3([1, 2, 3]) → [3, 3, 3]
+    maxEnd3([11, 5, 9]) → [11, 11, 11]
+    maxEnd3([2, 11, 3]) → [3, 3, 3]*/
+    public int[] maxEnd3(int[] nums) {
+        int firstElem = nums[0];
+        int lastElem = nums[nums.length - 1];
+        int largest = Math.max(firstElem, lastElem);
+        Arrays.fill(nums, largest);
+        return nums;
+    }
+
+    /*Given an array of ints, return the sum of the first 2 elements in the array. If the array
+    length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+
+    sum2([1, 2, 3]) → 3
+    sum2([1, 1]) → 2
+    sum2([1, 1, 1, 1]) → 2*/
+    public int sum2(int[] nums) {
+        return switch (nums.length) {
+            case 0 -> 0;
+            case 1 -> nums[0];
+            default -> nums[0] + nums[1];
+        };
     }
 }
